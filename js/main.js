b2b91ba08a5bd06e191276b6405332c5975950e6
Chris Minnick <chris@minnick.com>
2016-07-31 09:38:53 -0700

    expanding examples

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-var headerStyle = 'color:red;font-size:24px;font-family:sans-serif';
-var homeDivStyle = 'width:100%;border:1px solid black';
+var pageStyle = 'margin: 0px; background-color:#fff';
 
-var homePage = element('div',homeDivStyle,
-                            element('h1',headerStyle,'hello, world!'));
+var headerH1Style = 'color:red;font-size:24px;font-family:sans-serif;';
+var headerDivStyle = 'width:100%;border:1px solid black;text-align:center;';
 
-document.getElementById('app').innerHTML = homePage;
+var pageHeader = element('div',headerDivStyle,
+                            element('h1',headerH1Style,'hello, world!'));
+
+
+var leftNavDivStyle = 'width: 300px; float:left; border: 1px solid black';
+var leftNavListStyle = 'margin: 0px; padding: 0px; display:table; list-style: none; width: 100%; background:yellow;';
+var leftNavListItemStyle = 'display: table-cell;';
+var leftNavLinkStyle = 'color: #fff;text-decoration: none;padding: 0 10px 0;height: 20px;line-height: 20px;display: block;text-align: center;background: blue;';
+
+var leftNav = element('div',leftNavDivStyle,
+                            element('ul',leftNavListStyle,
+                                element('li',leftNavListItemStyle,
+                                    element('a',leftNavLinkStyle,
+                                        "first link"))));
+
+var homePage = element('div',pageStyle,pageHeader + leftNav);
+
+render(homePage,'app');
 
 function element (name,style,content){
     return '<' + name + ' style="' + style + '">' +  content + '</' + name + '>';
+}
+
+function render (element,location){
+    document.getElementById(location).innerHTML = element;
 }
