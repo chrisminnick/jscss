136fd9ee512df0c09a602093ff8b12a85f2fc3fa
Chris Minnick <chris@minnick.com>
2016-07-31 09:44:01 -0700

    added more nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var pageStyle = 'margin: 0px; background-color:#fff';
+var pageStyle = 'font-family: sans-serif; margin: 0px; background-color:#fff';
 
-var headerH1Style = 'color:red;font-size:24px;font-family:sans-serif;';
+var headerH1Style = 'color:red;font-size:24px;';
 var headerDivStyle = 'width:100%;border:1px solid black;text-align:center;';
 
 var pageHeader = element('div',headerDivStyle,
@@ -10,13 +10,21 @@ var pageHeader = element('div',headerDivStyle,
 var leftNavDivStyle = 'width: 300px; float:left; border: 1px solid black';
 var leftNavListStyle = 'margin: 0px; padding: 0px; display:table; list-style: none; width: 100%; background:yellow;';
 var leftNavListItemStyle = 'display: table-cell;';
-var leftNavLinkStyle = 'color: #fff;text-decoration: none;padding: 0 10px 0;height: 20px;line-height: 20px;display: block;text-align: center;background: blue;';
+var leftNavLinkStyle = 'color: #fff;text-decoration: none;padding: 0 10px 0;height: 40px;line-height: 40px;display: block;text-align: center;background: blue;';
 
 var leftNav = element('div',leftNavDivStyle,
                             element('ul',leftNavListStyle,
                                 element('li',leftNavListItemStyle,
                                     element('a',leftNavLinkStyle,
-                                        "first link"))));
+                                        "first link")+
+                                    element('a',leftNavLinkStyle,
+                                        "second link")+
+                                    element('a',leftNavLinkStyle,
+                                        "third link")+
+                                    element('a',leftNavLinkStyle,
+                                        "fourth link"))));
+
+
 
 var homePage = element('div',pageStyle,pageHeader + leftNav);
 
